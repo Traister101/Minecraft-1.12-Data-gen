17aa0541f517fb9aec9f56c43e145f97a7c0c3f5
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-03-30 01:05:21 -0700

    More and fancier data gen

--- MC_Asset_Generators.py
+++ MC_Asset_Generators.py
@@ -35,10 +35,11 @@ class Blockstate:
     variants: dict[str, Variant]
     uvLock: bool
 
-    def __init__(self, textures: dict[str, str], variants: dict[str, Variant], uvLock = False):
+    def __init__(self, name: str, textures: dict[str, str], variants: dict[str, Variant], uvLock = False):
         self.textures = textures
         self.variants = variants
         self.uvLock = uvLock
+        self.write(name)
 
     def dict_build(self) -> dict[str, Union[str, int, bool]]:
         blockState: dict[str, any] = {
@@ -46,7 +47,8 @@ class Blockstate:
             "defaults": {
                 "textures": self.textures
             },
-            "variants": {}}
+            "variants": {}
+        }
 
         if self.uvLock:
             blockState["defaults"]["uvlock"] = self.uvLock
@@ -61,6 +63,41 @@ class Blockstate:
         os.makedirs(os.path.dirname(file), exist_ok=True)
         with open(file, "w") as file:
             json.dump(self.dict_build(), file, indent=2)
+
+
+class Model:
+    parent: str
+    textures: dict[str, str]
+
+    def __init__(self, name: str, textures: dict[str, str], parent: str):
+        self.textures = textures
+        self.parent = parent
+        self.write(name)
+
+    def write(self, name: str):
+        file = os.path.join("models", name) + ".json"
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, "w") as file:
+            json.dump({
+                "parent": self.parent,
+                "textures": self.textures},
+                file, indent=2)
+
+
+class Item(Model):
+    def __init__(self, name: str, *textures: Union[list[str], tuple[str, ...], str]):
+        texturesDict: dict[str, str] = {}
+        for index, texture in enumerate(textures):
+            texturesDict[f"layer{index}"] = texture
+        super().__init__(f"item/{name}", texturesDict, "item/generated")
+
+
+class Block(Model):
+
+    def __init__(self, name: str, textures: Union[dict[str, str], str], parent: str = "block/cube_all"):
+        if type(textures) is str:
+            textures = {"all": textures}
+        super().__init__(f"block/{name}", textures, parent)
 
 
 STAIR_VARIANTS: dict[str, Variant] = {
@@ -116,7 +153,21 @@ STAIR_VARIANTS: dict[str, Variant] = {
     "facing=west,half=top,shape=inner_left": Variant("outer_stairs", (180, 180, 0)),
 }
 
-os.chdir("test")
-Blockstate({"top": "rnt:blocks/road/test",
-            "bottom": "rnt:blocks/test",
-            "side": "rnt:blocks/test"}, STAIR_VARIANTS, True).write("test")
+
+class Stairs(Blockstate):
+
+    def __init__(self, name: str, textures: Union[dict[str, str], str]):
+        if type(textures) is str:
+            textures = {"top": textures, "bottom": textures, "side": textures}
+        super().__init__(name, textures, STAIR_VARIANTS, True)
+
+
+if __name__ == "__main__":
+    os.chdir("test")
+    Blockstate("test_blockstate", {"top": "blocks/test",
+                                   "bottom": "blocks/test",
+                                   "side": "blocks/test"}, STAIR_VARIANTS, True)
+    Stairs("test_stairs", "blocks/test")
+    Model("test_model", {"test": "blocks/test"}, "block/cube_all")
+    Item("test_item", "items/test")
+    Block("test_block", "blocks/test")
