bc1cc22db30764c39b8ebf2e9a113bbed2ae8334
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-10-12 03:32:18 -0700

    Bump Version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 
-setup(name="MinecraftDataGen", version="1.0", author="Traister101", packages=["MinecraftDataGen"],
+setup(name="MinecraftDataGen", version="1.0.1", author="Traister101", packages=["MinecraftDataGen"],
       url="https://github.com/Traister101/Minecraft-1.12-Data-gen",
       description="Python package to help in generating Minecraft 1.12 assets needed in modding or resource pack creation")
